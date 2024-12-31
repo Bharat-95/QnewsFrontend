7fc4a7ca36e6cf264437b35616e6b6a7eccf01ac
G7cars <g7selfdrivecars@gmail.com>
2024-12-31 17:35:09 +0530

    a

--- components/Trending.js
+++ components/Trending.js
@@ -144,7 +144,7 @@ const Trending = () => {
   <div className="mb-2"> {/* Reduced margin-bottom to bring ad closer to title */}
     <ins
       className="adsbygoogle"
-      style={{ display: "block", width:"400px", height:"200px"}}
+      style={{ display: "block", width:"400px", height:"20px"}}
       data-ad-format="autorelaxed"
       data-ad-client="ca-pub-5147970592590624"
       data-ad-slot="7384828746"
